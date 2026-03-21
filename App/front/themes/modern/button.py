7729ecaf18ad196b77485e3b02cae9e0f78e3249
Leo <hereuser47@gmail.com>
2026-03-21 18:10:31 +0200

    new button themes

---
+++ App/front/themes/modern/button.py
@@ -0,0 +1,9 @@
+STYLE = {
+    "bd": 0,
+    "relief": "flat",
+    "bg": "#00d4ff",
+    "fg": "white",
+    "activebackground": "#008fb3",
+    "font": ("Arial", 10, "bold"),
+    "width": 20
+}
---
+++ App/front/themes/retro/button.py
@@ -0,0 +1,9 @@
+STYLE = {
+    "bd": 4,
+    "relief": "raised",
+    "bg": "#c0c0c0",
+    "fg": "black",
+    "activebackground": "#808080",
+    "font": ("Courier", 9, "bold"),
+    "width": 18
+}

b90dee6e206485dc5921ab8eeab9be09d894ab4d
Levoxyl <hereuser47@gmail.com>
2026-07-07 23:13:00 +0300

    feat&style: add a payload section and sockets for mapping requests ; Formating

--- App/front/themes/modern/src/qwebchannel.ts
+++ App/front/themes/modern/src/qwebchannel.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export class QWebChannel {
     transport: any;
-    execCallbacks:{[key: number]: (data: any) => void};
+    execCallbacks: { [key: number]: (data: any) => void };
     execId: number;
-    objects: {[key: string]: any};
+    objects: { [key: string]: any };
 
     constructor(transport: any, initCallback: (channel: QWebChannel) => void) {
         this.transport = transport;
@@ -13,20 +13,57 @@ export class QWebChannel {
 
         this.transport.onmessage = (message: any) => {
             const data = JSON.parse(message.data);
-            if (data.type == 1){                //prop update
-                for (const  i in data.properties){
+
+            if (data.type === 1) { // Properties update
+                for (const i in data.properties) {
                     const prop = data.properties[i];
-                    this.objects[prop.object].wrappedProperties[prop.property] = prop.value;
+                    if (this.objects[prop.object]) {
+                        this.objects[prop.object].wrappedProperties[prop.property] = prop.value;
+                    }
                 }
-            } else if (data.type == 2){         //response
+            } else if (data.type === 2) { // Method response
                 const callback = this.execCallbacks[data.id];
-                if (callback){
+                if (callback) {
                     delete this.execCallbacks[data.id];
                     callback(data.response);
                 }
+            } else if (data.type === 5) { // Initialization payload containing backend objects mappings
+                for (const name in data.objectData) {
+                    this.objects[name] = this.createQtObject(name, data.objectData[name]);
+                }
+                // Invoke callback only after objects are fully registered and built
+                initCallback(this);
             }
         };
-        this.transport.send(JSON.stringify({type: 0})); //Init connection   
-        initCallback(this);                             //Expose backend hook
+
+        this.transport.send(JSON.stringify({ type: 0 })); // Request initialization handshake
+    }
+
+    createQtObject(name: string, signalMethodMap: any) {
+        const obj: any = { wrappedProperties: {} };
+        // Map methods directly to invocation packets sent down the socket transport
+        if (signalMethodMap.methods) {
+            for (let i = 0; i < signalMethodMap.methods.length; i++) {
+                const methodName = signalMethodMap.methods[i][0];
+                obj[methodName] = (...args: any[]) => {
+                    let callback: ((data: any) => void) | null = null;
+                    if (args.length > 0 && typeof args[args.length - 1] === 'function') {
+                        callback = args.pop();
+                    }
+                    this.execId++;
+                    if (callback) {
+                        this.execCallbacks[this.execId] = callback;
+                    }
+                    this.transport.send(JSON.stringify({
+                        type: 3,
+                        id: this.execId,
+                        object: name,
+                        method: methodName,
+                        args: args
+                    }));
+                };
+            }
+        }
+        return obj;
     }
 }
